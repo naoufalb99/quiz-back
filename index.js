ecb6f9192f94fed3c460095456118e564e85bcfd
Naoufal BIDARI <naoufal@alphorm.com>
2022-11-03 18:27:00 +0100

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./src/routes')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-app.use(routes)
+app.use('/api', routes)
 
 app.use((req, res, next) => {
   res.status(404).send({ message: 'Not found', code: 404 })
